6ed8f828b09e9b4620d59d6dedd1b0a946ac5cc9
ascalo19 <ascalo19@ascalo19.net>
2016-01-29 17:01:15 +0100

    Now handle spam

--- src/main/java/ascalo19/imap2smtp/Application.java
+++ src/main/java/ascalo19/imap2smtp/Application.java
@@ -1,48 +1,66 @@
 package ascalo19.imap2smtp;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.mail.ImapIdleChannelAdapter;
 import org.springframework.integration.mail.ImapMailReceiver;
+import org.springframework.integration.transformer.HeaderEnricher;
+import org.springframework.integration.transformer.support.HeaderValueMessageProcessor;
+import org.springframework.integration.transformer.support.StaticHeaderValueMessageProcessor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.SubscribableChannel;
 import org.springframework.messaging.support.ExecutorSubscribableChannel;
 
+import javax.mail.Address;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
+
+	private static final Log log = LogFactory.getLog(HeaderEnricher.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
-	public SubscribableChannel messageChannel() {
+	public SubscribableChannel messageOutput() {
 		SubscribableChannel result = new ExecutorSubscribableChannel();
 		result.subscribe(new MessageHandler() {
 			@Override
 			public void handleMessage(Message<?> message) throws MessagingException {
+				//if (true) throw new MessagingException("REJECT TEST");
 				try {
 					MimeMessage email = (MimeMessage) message.getPayload();
-					System.out.println(email.getSubject());
-					String[] r = email.getHeader("Received");
-					for (int i = r.length - 1; i >= 0; i--) {
-						if (StringUtils.contains(r[i], "for ")) {
-							System.out.println(StringUtils.substringBetween(r[i], "for ", " "));
+					if (message.getHeaders().containsKey("SPAM")) {
+						email.setSubject("[SPAM] " + email.getSubject());
+					}
+					Address[] recipients = email.getAllRecipients();
+					String[] receivedHeaders = email.getHeader("Received");
+					for (int i = receivedHeaders.length - 1; i >= 0; i--) {
+						if (StringUtils.contains(receivedHeaders[i], "for ")) {
+							String recipient = StringUtils.substringBetween(receivedHeaders[i], "for ", " ");
+							recipients = new Address[]{new InternetAddress(StringUtils.remove(recipient, ';'))};
 							break;
 						}
 					}
-					//Address to = new InternetAddress("<pascal.lovy@iteral.ch>");
-					Test to + cc + cci
-					mailForwarder().forward(email.getAllRecipients(), email);
+					log.info("Delivering to " + Arrays.toString(recipients) + " message " + email.getSubject());
+					smtpForwarder().forward(recipients, email);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,36 +70,77 @@ public class Application {
 	}
 
 	@Bean
-	public ImapMailReceiver imapReceiver() {
-		Properties conf = new Properties();
-		conf.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		conf.setProperty("mail.imap.socketFactory.fallback", "false");
-		conf.setProperty("mail.store.protocol", "imaps");
-		conf.setProperty("mail.debug", "false");
-		ImapMailReceiver result = new ImapMailReceiver("imaps://imap.gmail.com:993/inbox");
+	public ImapMailReceiver imapInboxReceiver() {
+		ImapMailReceiver result = new ImapMailReceiver("imaps://imap.gmail.com:993/inbox2");
 		result.setShouldDeleteMessages(true);
-		result.setJavaMailProperties(conf);
-		result.setJavaMailAuthenticator(new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("iteral.iteral@gmail.com", "1pwd4Mails");
-			}
-		});
+		result.setJavaMailProperties(javaMailProperties());
+		result.setJavaMailAuthenticator(javaMailAuthenticator());
 		return result;
 	}
 
 	@Bean
-	public ImapIdleChannelAdapter imapAdapter() {
-		ImapIdleChannelAdapter result = new ImapIdleChannelAdapter(imapReceiver());
-		result.setOutputChannel(messageChannel());
+	public ImapMailReceiver imapSpamReceiver() {
+		ImapMailReceiver result = new ImapMailReceiver("imaps://imap.gmail.com:993/Spam2");
+		result.setShouldDeleteMessages(true);
+		result.setJavaMailProperties(javaMailProperties());
+		result.setJavaMailAuthenticator(javaMailAuthenticator());
 		return result;
 	}
 
 	@Bean
-	public JavaMailForwarder mailForwarder() {
+	public ImapIdleChannelAdapter messageInboxInput() {
+		ImapIdleChannelAdapter result = new ImapIdleChannelAdapter(imapInboxReceiver());
+		result.setOutputChannel(messageOutput());
+		return result;
+	}
+
+	@Bean
+	public ImapIdleChannelAdapter messageSpamInput() {
+		ImapIdleChannelAdapter result = new ImapIdleChannelAdapter(imapSpamReceiver());
+		result.setOutputChannel(messageSpamTransfer());
+		return result;
+	}
+
+	@Bean
+	public DirectChannel messageSpamTransfer() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	@Transformer(inputChannel = "messageSpamTransfer", outputChannel = "messageOutput")
+	public HeaderEnricher enrichSpamHeader() {
+		Map<String, ? extends HeaderValueMessageProcessor<?>> headersToAdd = Collections.singletonMap("SPAM", new StaticHeaderValueMessageProcessor<>(Boolean.TRUE));
+		HeaderEnricher enricher = new HeaderEnricher(headersToAdd);
+		return enricher;
+	}
+
+	@Bean
+	public JavaMailForwarder smtpForwarder() {
 		JavaMailForwarder result = new JavaMailForwarder();
 		result.setHost("localhost");
 		result.setPort(2525);
+		return result;
+	}
+
+	@Bean
+	public Properties javaMailProperties() {
+		Properties result = new Properties();
+		result.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		result.setProperty("mail.imap.socketFactory.fallback", "false");
+		result.setProperty("mail.store.protocol", "imaps");
+		result.setProperty("mail.debug", "false");
+		return result;
+	}
+
+	@Bean
+	public Authenticator javaMailAuthenticator() {
+		Authenticator result = new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication("<EMAIL>", "<PASSWORD>");
+			}
+		};
 		return result;
 	}
 }
